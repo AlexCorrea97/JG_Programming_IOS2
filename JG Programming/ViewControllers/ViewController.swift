420480822ff2ed4d1f91e3b5e58ceaa4d8177105
Xchel Carranza <xchel.co@gmail.com>
2020-07-16 03:37:17 -0500

    diseño

--- JG Programming/ViewControllers/ViewController.swift
+++ JG Programming/ViewControllers/ViewController.swift
@@ -9,12 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let ADMIN_CODE:String = "AdminVC"
+    let TRAINING_CODE:String = "TRainingVC"
+    var segueLoginCode:String = ""
+    
+    @IBOutlet weak var preLoginView: UIView!
+    @IBOutlet weak var loginView: UIView!
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        //
+        preLogin()
     }
 
-
+    @IBAction func preLoginButton(_ sender: UIButton) {
+        login()
+    }
+    @IBAction func getAccountButton(_ sender: UIButton) {
+        
+    }
+    @IBAction func loginButton(_ sender: UIButton) {
+        if(userNameTextField.text == "jg"){
+            segueLoginCode = ADMIN_CODE
+        }else{
+            segueLoginCode = TRAINING_CODE
+        }
+        performSegue(withIdentifier: segueLoginCode, sender: self)
+    }
+    
+    private func preLogin(){
+        loginView.isHidden = true
+    }
+    private func login(){
+        preLoginView.isHidden = true
+        loginView.isHidden = false
+    }
 }
 
