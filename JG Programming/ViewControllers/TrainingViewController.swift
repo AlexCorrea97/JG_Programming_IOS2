be82791df3026c3f6dc5a79bbaf23fedb6141a39
Xchel Carranza <xchel.co@gmail.com>
2020-07-15 22:21:05 -0500

    CARPETA DE LAS VISTAS

---
+++ JG Programming/ViewControllers/TrainingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TrainingViewController.swift
+//  JG Programming
+//
+//  Created by Xchel Alonso Carranza De La O on 15/07/20.
+//  Copyright © 2020 Xchel Alonso Carranza De La O. All rights reserved.
+//
+
+import UIKit
+
+class TrainingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
