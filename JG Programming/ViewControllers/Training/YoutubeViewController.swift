72c064f3466314219b1bdb6c01b32fd66b145d9f
Xchel Carranza <xchel.co@gmail.com>
2020-07-17 23:24:20 -0500

    rediseño de vistas

---
+++ JG Programming/ViewControllers/Training/YoutubeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  YoutubeViewController.swift
+//  JG Programming
+//
+//  Created by Xchel Alonso Carranza De La O on 17/07/20.
+//  Copyright © 2020 Xchel Alonso Carranza De La O. All rights reserved.
+//
+
+import UIKit
+
+class YoutubeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
