6976691aec6436a6003965ce53977f2e87bfc130
Alexcorrea <alexcorrea1997@outlook.com>
2020-07-20 23:20:52 -0700

    arreglando el desmadre de xchel

--- JG Programming/ViewControllers/Programming/ProgrammingViewController.swift
+++ JG Programming/ViewControllers/Programming/ProgrammingViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ProgrammingViewController: UIViewController{
+    
+    
+    
+    
     /*func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return days.count
     }
@@ -21,20 +25,21 @@ class ProgrammingViewController: UIViewController{
     
     @IBOutlet weak var collectionView: UICollectionView!
     
-    private let days:[String] = ["Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","domingo"]
+    private let days = ["Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","domingo"]
     private let DAY_COLLECTION_VIEW:String = "DayProgrammingCollectionViewCell"
     private let DAY_VIEW:String = "dayView"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         collectionView.dataSource = self
+        collectionView.delegate = self
         collectionView.register(UINib(nibName: DAY_COLLECTION_VIEW, bundle:nil), forCellWithReuseIdentifier:DAY_VIEW)
     }
 
 }
 
-extension ViewController: UICollectionViewDataSource{
+extension ProgrammingViewController: UICollectionViewDataSource{
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return days.count
     }
@@ -44,5 +49,15 @@ extension ViewController: UICollectionViewDataSource{
         cell!.daysOFWeek.text = days[indexPath.row]
         return cell!
     }
+    
+}
+
+extension ProgrammingViewController: UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(width: collectionView.bounds.width, height: collectionView.bounds.height)
+    }
+
+    
     
 }
